a3dd5ca9f1b844806cbe393099aa110b7f87afcb
Will Trojniak <william.trojniak@gmail.com>
2024-12-29 16:00:06 -0500

    Site revamp - Landing and Contact pages

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 30%, 70%, 100%': { transform: 'rotate(0deg)' },
+          '40%, 60%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 8s ease-in-out infinite'
+      }
+    },
+  },
+  plugins: [],
+}
+
