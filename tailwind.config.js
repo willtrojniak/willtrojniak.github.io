d54dbf53494e2927a0e02040b0d66e9cd76909b9
Will Trojniak <william.trojniak@gmail.com>
2024-12-29 16:03:04 -0500

    Rename src to docs
    Updated for gh pages compatibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./docs/**/*.{html,js}"],
   theme: {
     extend: {
       keyframes: {
